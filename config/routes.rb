b0726469de0d53b9bff012532d55706b15852576
shalinimalipatil <shalinideshmukh1991@gmail.com>
2017-05-08 02:14:54 -0700

    deshhhh

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'students/new'
+
+  get 'students/index'
+
+  get 'students/edit'
+
+  get 'students/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
